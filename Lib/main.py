e42d8af2ba0e1e524a0681c60e168a1207fc5120
Lisa-Mays <lisamaysm@gmail.com>
2020-09-13 00:04:05 -0400

    Changes to how calculate_cost function works

--- Lib/main.py
+++ Lib/main.py
@@ -1,44 +1,57 @@
 # This program will calculate the cost to carpet a room
 # given the height and width of the room
-# and the choice of carpet grades
+# and the users choice of 3 carpet grades
 
-# Set price per square foot for 3 grades of carpet
+# Set price per square foot for 3 grades of carpet.
+# Good grade carpet is 1.25
+# Better grade carpet is 2.75
+# Best grade carpet is 3.95
 carpet_good = 1.25
 carpet_better = 2.75
 carpet_best = 3.95
 
 # Prompt for width and height of the room in feet
-height = int(input("How many feet is the room height?: "))
-width = int(input("How many feet is the room width?: "))
+height = float(input("How many feet is the room height?: "))
+width = float(input("How many feet is the room width?: "))
 
-# Pick Grade of Carpet (3 choices) good, better, best
+# Error checking for positive numbers for room height and width
+if height <= 0:
+    print("Please enter a room height greater than zero!")
+    height = float(input("How many feet is the room height?: "))
+elif width <= 0:
+    print("Please enter a room width greater than zero!")
+    width = float(input("How many feet is the room width?: "))
+
+# Prompt for grade of Carpet (3 choices) good, better, best
 print(f"""
 There are three grades of carpet to choose from.
 The Good carpet costs $ {carpet_good} a square foot. It's pretty Good!
 The Better carpet costs $ {carpet_better} a square foot. It's Better!
 The Best carpet costs $ {carpet_best} a square foot. It's the Best!
 """)
-carpet_grade = input("Please choose a grade of carpet. (good, better or best): ")
-if carpet_grade.lower() == 'good':
-    carpet_grade = carpet_good
-elif carpet_grade.lower() == 'better':
-    carpet_grade = carpet_better
-elif carpet_grade.lower() == 'best':
-    carpet_grade = carpet_best
-else:
-    print("Invalid input.  Please restart program.")
-    exit()
-
-# Compute cost Function that multiplies height and width by the carpet cost
-
-def calculate_cost():
-    room_cost = width * height * carpet_grade
-    return room_cost
+carpet_grade_input = input("Please choose a grade of carpet. (good, better or best): ")
 
 
-# Write cost to carpet room
-room_cost = calculate_cost()
-print(f"It will cost $ {room_cost} to carpet your room.")
+# Function calculate_cost that accepts width, height, and grade as input
+# Compute the room carpet cost by multiplying height and width by the carpet grade cost
+def calculate_cost(carpet_width, carpet_height, carpet_grade_type):
+    carpet_grade = 0
+    if carpet_grade_type.lower() == 'good':
+        carpet_grade = carpet_good
+    elif carpet_grade_type.lower() == 'better':
+        carpet_grade = carpet_better
+    elif carpet_grade_type.lower() == 'best':
+        carpet_grade = carpet_best
+    else:
+        print("Invalid input.  Please restart program.")
+        exit()
+    room_cost_calc = (carpet_width * carpet_height * carpet_grade)
+    return room_cost_calc
+
+
+# Call calculate_cost function and Write cost to carpet room
+room_cost = calculate_cost(width, height, carpet_grade_input)
+print("It will cost $ " + ("{:.2f}".format(room_cost)) + " to carpet your room.")
 
 
 
